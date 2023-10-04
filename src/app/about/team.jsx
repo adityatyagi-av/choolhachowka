6d97215030eff48a5f5bbab51863f6ca0208a177
MUKUL <mukul.2226csit1115@kiet.edu>
2023-10-04 17:48:17 +0530

    created team page

--- src/app/about/team.jsx
+++ src/app/about/team.jsx
@@ -1,9 +1,58 @@
+
 import React from 'react'
 
 const Team = () => {
   return (
     <div>
+      <div className='text-center'>
+            <p>Lors optio rporis.</p>
+            </div>
+      <h2 className="text-2xl mb-4 text-center">MEET OUR TEAM MEMBERS</h2>
       
+      <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi necessitatibus magnam eaque modi? Iusto, numquam ad quam et poss opt deserunt cum, illum nihil quasi oditta</p>
+
+<div className='justify-around'>
+      <div className='flex flex-row justify-center'>
+        
+      <div>
+        <div className='flex-col h-350px w-500px'>
+        <img src="https://th.bing.com/th/id/OIP.Rs8VqiNkHCVFPiweukeESQHaE7?pid=ImgDet&rs=1" alt="image 1" />
+        <p>Lorem ipsum dolor sit amconsectetur adipisicingt, odit excepturi! Error, repudiin alias tempore similique sum?</p>
+        </div>
+        <div className='flex justify-center'>
+      <a href="https://twitter.com/home" target='blank'> 
+      <img className="w-6 h-6 mr-2"  src="https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png" alt="twitter logio" /></a>
+      <a href="https://www.instagram.com/?hl=en" target='blank'><img className="w-6 h-6 mr-2"  src="https://www.northstar-alliance.org/wp-content/uploads/2019/08/insta-logo.png" alt="instagram" /></a>
+      <a href="https://www.linkedin.com/" target='blank'><img className="w-6 h-6 mr-2"  src="https://th.bing.com/th/id/OIP.w_zDkEJ9aLiWR-g0rff8hwHaHa?pid=ImgDet&rs=1" alt="linkedin" /></a>
+      <a href="https://www.facebook.com/" target='blank'><img className="w-9 h-6 mr-2"  src="https://th.bing.com/th/id/OIP.zkkdY1CFVdOkJgqKkYX2bQHaFL?pid=ImgDet&rs=1" alt="facebook" /></a>
+      </div>
+        </div>
+        <div>
+          <div className='flex-col h-350px w-500px'>
+          <img src="https://th.bing.com/th/id/OIP.Rs8VqiNkHCVFPiweukeESQHaE7?pid=ImgDet&rs=1" alt="Image 2" />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At tempora, reiciendm sint molestiae consequuntur!</p>
+          </div>
+          <div className='flex mx-10 justify-center'>
+        <a href="https://twitter.com/home" target='blank'> <img  className="w-6 h-6 mr-2"    src="https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png" alt="twitter logo " /></a>
+        <a href="https://www.instagram.com/?hl=en" target='blank'><img className="w-6 h-6 mr-2"  src="https://www.northstar-alliance.org/wp-content/uploads/2019/08/insta-logo.png" alt="instagram" /></a>
+        <a href="https://www.linkedin.com/" target='blank'><img className="w-6 h-6 mr-2"  src="https://th.bing.com/th/id/OIP.w_zDkEJ9aLiWR-g0rff8hwHaHa?pid=ImgDet&rs=1" alt="linkedin" /></a>
+      <a href="https://www.facebook.com/" target='blank'><img className="w-9 h-6 mr-2"  src="https://th.bing.com/th/id/OIP.zkkdY1CFVdOkJgqKkYX2bQHaFL?pid=ImgDet&rs=1" alt="facebook" /></a>
+      </div>
+        </div>
+        <div>
+          <div className=' flex-col h-350px w-500px'>
+          <img src="https://wilkinsuranceagency.com/wp-content/uploads/2020/02/businessman-eager.png" alt="image -3 "/>
+          <p>Lorejimimmet consectetur adipisicing elit. Maxime corporisod distinctio alias ea.</p>
+          </div>
+          <div  className='flex mx-10 justify-center'>
+          <a href="https://twitter.com/home" target='blank'><img className="w-6 h-6 mr-2"  src="https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png" alt="twitter" /></a>
+          <a href="https://www.instagram.com/?hl=en" target='blank'><img  className="w-6 h-6 mr-2"  src="https://www.northstar-alliance.org/wp-content/uploads/2019/08/insta-logo.png" alt="instagram" /></a>
+          <a href="https://www.linkedin.com/" target='blank'><img className="w-6 h-6 mr-2" src="https://th.bing.com/th/id/OIP.w_zDkEJ9aLiWR-g0rff8hwHaHa?pid=ImgDet&rs=1" alt="linkedin" /></a>
+      <a href="https://www.facebook.com/" target='blank'><img className="w-9 h-6 mr-2"  src="https://th.bing.com/th/id/OIP.zkkdY1CFVdOkJgqKkYX2bQHaFL?pid=ImgDet&rs=1" alt="facebook" /></a>
+      </div>
+        </div>
+        </div>
+        </div>
     </div>
   )
 }
