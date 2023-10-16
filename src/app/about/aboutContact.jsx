21a38fffc91da395e4f94e8159a6eec23349169c
YashSharma2129 <yash.2226it1188@kiet.edu>
2023-10-16 10:43:20 +0530

    added aboutContact section

--- src/app/about/aboutContact.jsx
+++ src/app/about/aboutContact.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const AboutContact = () => {
   return (
-    <div>
-      
+    <div className="bg-blue-500 mt-6 text-white py-10">
+      <div className="container mx-auto text-center">
+        <h2 className="text-2xl font-bold mb-4">
+          Still Have Some Questions Left?
+        </h2>
+        <p className="mb-4">Call us Tel: 855-456-7634</p>
+        <p className="mb-4">
+          Feel free to contact our support team to learn more about the services
+          provided by us and multiple offers for you.
+        </p>
+        <button className="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-blue-200 hover:text-blue-600 transition duration-300 ease-in-out">
+          Contact Us
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AboutContact
